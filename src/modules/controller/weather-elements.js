2f8cc7b9f3ab025a8cec96f5553f8b5c061fca42
nico-von <131730901+nico-von@users.noreply.github.com>
2024-04-23 16:35:27 +0800

    Make a module containing elements of the app

---
+++ src/modules/controller/weather-elements.js
@@ -0,0 +1,26 @@
+export const weatherCard = {
+    weatherType: document.querySelector("#weather-type"),
+    weatherLocation: document.querySelector("#weather-location"),
+    weatherDate: document.querySelector("#weather-date"),
+    weatherTemp: document.querySelector("#weather-temp")
+}
+
+export const weatherCardBtns = {
+    celciusBtn: document.querySelector("#display-type-celcius"),
+    fahBtn: document.querySelector("#display-type-fah"),
+    searchInput: document.querySelector("#search-textbox"),
+    searchBtn: document.querySelector("#search-button")
+}
+
+export const weatherDetails = {
+    feelsLike: document.querySelector("#feels-like"),
+    humidity: document.querySelector("#humidity"),
+    rainChance: document.querySelector("#rain-chance"),
+    windSpeed: document.querySelector("#wind-speed")
+}
+
+export const forecastElements = {
+    dailyBtn: document.querySelector("#daily-forecast"),
+    hourlyBtn: document.querySelector("#hourly-forecast"),
+    forecastContent: document.querySelector("#forecast-content")
+}
