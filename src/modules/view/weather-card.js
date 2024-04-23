637d9f6d3668c00fb3abf08769f670d05fb4ad94
nico-von <131730901+nico-von@users.noreply.github.com>
2024-04-23 18:38:55 +0800

    Add view manipulator for weather details and cards

--- src/modules/view/weather-card.js
+++ src/modules/view/weather-card.js
@@ -7,10 +7,26 @@ export function setWeatherCard(
     weatherDate,
     weatherTemp) {
 
-    const {type, icon, location, date, currentTempC, currentTempF} = weatherObj;
+    const { type, icon, location, date, currentTempC, currentTempF } = weatherObj;
     weatherType.textContent = type;
     weatherTypeIcon.src = icon;
     weatherLocation.textContent = location;
     weatherDate.textContent = date;
     weatherTemp.textContent = isCelcius ? currentTempC : currentTempF;
+}
+
+
+export function setWeatherDetails(
+    isCelcius,
+    weatherObj,
+    feelsLike,
+    humidity,
+    rainChance,
+    windSpeed
+) {
+    const { heatIndexC, heatIndexF, humidity, rainChance, windSpeed } = weatherObj;
+    feelsLike.textContent = isCelcius ? heatIndexC : heatIndexF;
+    humidity.textContent = humidity;
+    rainChance.textContent = rainChance;
+    windSpeed.textContent = windSpeed;
 }
